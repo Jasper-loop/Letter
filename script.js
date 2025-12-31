044bc7929d75c6ce0c4bba46af7ba9ebee7acf7b
Ian Jasper <184511754+Jasper-loop@users.noreply.github.com>
2026-01-01 00:42:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 const envelope = document.getElementById("envelope");
 const music = document.getElementById("music");
-const letter = document.getElementsByClassName("letter")[0];
 
 let opened = false;
 
-function openEnvelope() {
+envelope.addEventListener("pointerdown", function () {
   if (opened) return;
 
   opened = true;
   envelope.classList.add("open");
+
+  music.currentTime = 0;
   music.play();
-  letter.position
-}
-
-/* Desktop click */
-envelope.addEventListener("click", openEnvelope);
-
-/* Mobile touch */
-envelope.addEventListener("touchstart", openEnvelope);
+});
